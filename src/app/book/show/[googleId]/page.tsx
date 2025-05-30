374f43c056ed1638b487ea612a3b89fea68ee9ce
mayazaki98 <n.yamamoto98@gmail.com>
2025-05-31 00:45:25 +0900

    vercel のビルドエラー対応

--- src/app/book/show/[googleId]/page.tsx
+++ src/app/book/show/[googleId]/page.tsx
@@ -1,10 +1,16 @@
 import { prisma } from '@/utils/prisma';
 import BookShow from '@/components/BookShow';
 
-export default async function BookShowPage({ params }: { params: { googleId: string } }) {
+type BookShowPageProps = {
+    params: { googleId: string };
+};
+
+export default async function BookShowPage({ params }: BookShowPageProps) {
+    const { googleId } = params;
+
     // Bookデータをprismaから取得
     const book = await prisma.book.findUnique({
-        where: { googleId: params.googleId },
+        where: { googleId: googleId },
         include: {
             bookPosts: {
                 take: 10,
